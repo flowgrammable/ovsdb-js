204728aad8d8dc54ce1a0b295301e9283edf57fd
Colton Chojnacki <coltonchojnacki@gmail.com>
2015-04-21 08:28:22 -0500

    update

--- examples/ovsdb-cli.js
+++ examples/ovsdb-cli.js
@@ -1,15 +1,18 @@
 #!/usr/bin/env node
 
-var config = require('./config.json');
+var config = require('./config.js');
 var ovsdb  = require('../lib/ovsdb.js');
+var ovs    = require('../lib/ovs.js');
 var net = require('net');
 var program = require('commander');
 
 
+console.log(config);
 program
   .version('0.0.1')
   .option('-l --list', 'List databases')
   .option('-s --schema [db_name]', 'Get database schema')
+  .option('-b --bridges', 'list bridges')
   .parse(process.argv);
 
 if(!process.argv.slice(2).length){
@@ -17,23 +20,32 @@ if(!process.argv.slice(2).length){
 } else {
   var client = new net.Socket();
   client.connect(config.port, config.ip, function(){
-    var ovsdbcli = new ovsdb.OVSDB(client);
+    var ovsdbcli = new ovsdb.OVSDB(client, rxHandler);
     if(program.list){
       ovsdbcli.list(output);
     } else if(program.schema){
       ovsdbcli.get(program.schema, output);
-    }
+    } else if(program.bridges){
+      ovsdbcli.monitor('Open_vSwitch', ovs.listBridges(), output);
+   }
 });
 
 }
 
-function output(err, res){
+var rxHandler = function(err, res){
   if(err){
-    console.log(JSON.stringify(err));
+    console.log(err);
   } else if(res) {
-    console.log(JSON.stringify(res));
+    console.log(res);
   }
-  client.end();
+}
+
+var output = function(err, res){
+  if(err){
+    console.log(err);
+  } else if(res) {
+    console.log(res);
+  }
 }
 
 
