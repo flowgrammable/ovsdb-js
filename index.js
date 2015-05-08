2913787dfd10850ae1f9414bee02e349cb64d4e6
Colton Chojnacki <coltonchojnacki@gmail.com>
2015-05-08 12:56:46 -0500

    update

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 exports.OVSDB = require('./lib/ovsdb');
 exports.Operations = require('./lib/operations');
 exports.Primitives = require('./lib/primitives');
+exports.Utilities  = require('./lib/ovsdb-util');
