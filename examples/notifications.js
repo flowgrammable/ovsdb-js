deb61779a2b11ba04ce123ba42f382cf54fc4d53
Colton Chojnacki <coltonchojnacki@gmail.com>
2015-05-22 10:31:18 -0500

    update

--- examples/notifications.js
+++ examples/notifications.js
@@ -6,9 +6,10 @@ var ovs      = require('../lib/ovs.js');
 var net      = require('net');
 var client   = new net.Socket();
 var schema   = require('../lib/schema');
+
 var cfg = {
   ovsdb: {
-     ip: '192.168.1.112',
+     ip: '192.168.1.120',
      port: 6640
   },
   jsonrpc: {
@@ -16,7 +17,21 @@ var cfg = {
         name: 'ovs',
         stream: process.stdout,
         level: 'debug'
-        }
+     }
+  }
+};
+
+var monitorRequests = {
+     'Open_vSwitch': { 
+       columns: ['bridges', 'cur_cfg', 'manager_options', 'ovs_version'] 
+     },
+     'Bridge': { 
+       columns: ['name', 'ports', 'controller', 'flow_tables', 'datapath_id', 'protocols'],
+       select: { initial: true, insert: true, delete: true, modify: true}
+     },
+     'Controller': { 
+       columns: ['target', 'connection_mode', 'is_connected', 'role'],
+       select: { initial: true, insert: true, delete: true, modify: true}
      }
 };
 var ovsdbcli = new ovsdb.OVSDB(cfg.jsonrpc, client);
@@ -27,32 +42,16 @@ client.connect(cfg.ovsdb.port, cfg.ovsdb.ip, function(){
     else {
       var sch = schema.fromJSON(res);
       var mir = new db.DBMirror(sch);
-      var vs;
-      var monTables = {
-        'Open_vSwitch': { 
-          columns: ['bridges', 'cur_cfg', 'manager_options', 'ovs_version'] 
-        },
-        'Bridge': { 
-          columns: ['name', 'ports', 'controller', 'flow_tables', 'datapath_id', 'protocols'],
-          select: { initial: true, insert: true, delete: true, modify: true}
-        },
-        'Controller': { 
-          columns: ['target', 'connection_mode', 'is_connected', 'role'],
-          select: { initial: true, insert: true, delete: true, modify: true}
-        }
-      };
-      ovsdbcli.monitor('Open_vSwitch', monTables, 
+      ovsdbcli.monitor('Open_vSwitch', monitorRequests, 
       function(err, res){
+        // Monitor Response Handler
         mir.updateFromJSON(res);
         console.log(mir.toString());
-        vs = new ovs.OVS(mir);
-        console.log(vs.toString());
       },
       function(err, res){
+        // Monitor Update Notification Handler
         mir.updateFromJSON(res);
         console.log(mir.toString());
-        vs.updateFromMirror(mir);
-        console.log(vs.toString());
       });
     }  
   });
